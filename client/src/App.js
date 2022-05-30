d672c0751b95f732b9cd41af4b43c56d2ad48424
reut-udler <86197308+reut-udler@users.noreply.github.com>
2022-05-30 08:44:30 +0300

    starting the app. calendar setup

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./App.css";
+import { getMonth } from "./utils";
+import CalendarHeader from "./components/CalendarHeader";
+import Sidebar from "./components/Sidebar";
+import Month from "./components/Month";
+
+function App() {
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  return (
+    <React.Fragment>
+      <div className="h-screen flex flex-columns">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currentMonth} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
