9720b6e8eb9ada08263d83152fd7edb976a5b2a5
reut-udler <86197308+reut-udler@users.noreply.github.com>
2022-05-30 14:01:09 +0300

    edit clendar header

--- client/src/components/CalendarHeader.js
+++ client/src/components/CalendarHeader.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
+import dayjs from "dayjs";
+
+import logo from "../assets/logo.png";
+import GlobalContext from "../context/GlobalContext";
 
 export default function CalendarHeader() {
-  return <div></div>;
+  const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+
+  const handleReset = () => {
+    setMonthIndex(dayjs().month());
+  };
+
+  const handlePrevMonth = () => {
+    setMonthIndex(monthIndex - 1);
+  };
+
+  const handleNextMonth = () => {
+    setMonthIndex(monthIndex + 1);
+  };
+
+  return (
+    <header className="px-4 py-2 flex items-center">
+      <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
+      <h1 className="mr-10 text-xl text-gray-500 font-bold">MediCalendar</h1>
+      <button className="border rounded py-2 px-4 mr-5" onClick={handleReset}>
+        Today
+      </button>
+      <button onClick={handlePrevMonth}>
+        <span className="material-icons-outlined curser-pointer text-gray-600 mx-2">
+          chevron_left
+        </span>
+      </button>
+      <button onClick={handleNextMonth}>
+        <span className="material-icons-outlined curser-pointer text-gray-600 mx-2">
+          chevron_right
+        </span>
+      </button>
+      <h2 className="ml-4 text-xl text-gray-500 font-bold">
+        {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
+      </h2>
+    </header>
+  );
 }
--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -1,6 +1,12 @@
 import React from "react";
+import dayjs from "dayjs";
 
 export default function Day({ day, rowIdx }) {
+  const getCurrentDayClass = () => {
+    return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+      ? "bg-blue-600 text-white rounded-full w-7"
+      : "";
+  };
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col text-center">
@@ -8,7 +14,10 @@ export default function Day({ day, rowIdx }) {
           <p className="text-sm mt-1">{day.format("ddd").toUpperCase()}</p>
         )}
 
-        <p className="text-sm p-1 my-1 text-center">{day.format("DD")}</p>
+        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
+          {" "}
+          {day.format("DD")}{" "}
+        </p>
       </header>
     </div>
   );
