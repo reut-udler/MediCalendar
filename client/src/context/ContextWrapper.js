7dd3be58ee10dde4c8cdeefe16a71020dc37cd7a
reut-udler <86197308+reut-udler@users.noreply.github.com>
2022-06-01 14:42:09 +0300

    changes

--- client/src/context/ContextWrapper.js
+++ client/src/context/ContextWrapper.js
@@ -4,8 +4,23 @@ import GlobalContext from "./GlobalContext";
 
 export default function ContextWrapper(props) {
   const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  const [smallCalendarMounth, setSmallCalendarMounth] = useState(null);
+
+  useEffect(() => {
+    if (smallCalendarMounth !== null) {
+      setMonthIndex();
+    }
+  }, [smallCalendarMounth]);
+
   return (
-    <GlobalContext.Provider value={{ monthIndex, setMonthIndex }}>
+    <GlobalContext.Provider
+      value={{
+        monthIndex,
+        setMonthIndex,
+        smallCalendarMounth,
+        setSmallCalendarMounth,
+      }}
+    >
       {props.children}
     </GlobalContext.Provider>
   );
--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -3,6 +3,8 @@ import React from "react";
 const GlobalContext = React.createContext({
   monthIndex: 0,
   setMonthIndex: (index) => {},
+  smallCalendarMounth: 0,
+  setSmallCalendarMonth: (index) => {},
 });
 
 export default GlobalContext;
